177e4fc31d9037a4e2cf274ec53c766dcd59d52f
Leandro Oniszczuk <leandro@indextank.com>
2011-06-10 18:00:47 -0300

    instant links for drupal

--- indextank/indextank_client.php
+++ indextank/indextank_client.php
@@ -22,6 +22,23 @@ function convert_to_map($array_object) {
     return $result;
 }
 
+/*
+ * Converts an array of 2 elements (which can be NULL) to a colon-separated string containing those elements.
+ * NULL elements are replaced by '*'
+ *
+ * Examples: 
+ *     map_range(array(2,4)) => "2:4";
+ *     map_range(array(NULL,3)) => "*:3";
+ *     map_range(array(5,NULL)) => "5:*";
+ *     map_range(array(NULL,NULL)) => "*:*";
+ *
+ *
+ * @param val: an array of 2 elements.
+ */
+function map_range($val) {
+    return sprintf("%s:%s",($val[0] == NULL ? "*": $val[0]), ($val[1] == NULL ? "*": $val[1]));
+}
+
 function api_call($method, $url, $params=array()) {
     $splits = parse_url($url);
     if (! empty($splits['scheme'])) { $scheme = $splits['scheme'].'://'; } else { throw new InvalidUrl("[".$url."]"); }
@@ -190,24 +207,64 @@ class IndexClient {
     }
 
     public function delete_index() {
-        api_call('DELETE', $this->index_url);
+        $res = api_call('DELETE', $this->index_url);
+        return $res->status;
     }
 
     public function add_document($docid, $fields, $variables = NULL) {
         /*
          * Indexes a document for the given docid and fields.
          * Arguments:
-         *     docid: unique document identifier
-         *     field: map with the document fields
+         *     docid: unique document identifier. A String no longer than 1024 bytes. Can not be NULL
+         *     fields: map with the document fields. field names and values MUST be UTF-8 encoded.
          *     variables (optional): map integer -> float with values for variables that can
          *                           later be used in scoring functions during searches.
          */
-        $data = array("docid" => $docid, "fields" => $fields);
-        if ($variables != NULL) {
-            $data["variables"] = convert_to_map($variables);
-        }
-        api_call('PUT', $this->docs_url(), $data);
+        $res =  api_call('PUT', $this->docs_url(), $this->as_document($docid, $fields, $variables));
+        return $res->status;
     }
+
+
+    public function add_documents($documents=array()) {
+        /*
+         * Indexes an array of documents. Each element (document) on the array needs to be an
+         * array, with 'docid', 'fields' and optionally 'variables' keys.
+         * The semantic is the same as IndexClient->add_document();
+         * Arguments:
+         *     documents: an array of arrays, each representing a document
+         */
+        $data = array();
+        foreach ($documents as $i => $doc_data) {
+            try {
+                // make sure docid is set
+                if (!array_key_exists("docid", $doc_data)) {
+                    throw new InvalidArgumentException("document $i lacks 'docid'");
+                }
+                $docid = $doc_data['docid'];
+                
+                // make sure fields is set
+                if (!array_key_exists("fields", $doc_data)) {
+                    throw new InvalidArgumentException("document $i lacks 'fields'");
+                }
+                $fields = $doc_data['fields'];
+                
+                // set $variables
+                if (!array_key_exists("variables", $doc_data)) {
+                    $variables = NULL;
+                } else {
+                    $variables = $doc_data['variables'];
+                }
+
+                $data[] = $this->as_document($docid, $fields, $variables);
+            } catch (InvalidArgumentException $iae) {
+                throw new InvalidArgumentException("while processing document $i: " . $iae->getMessage());
+            }
+        }
+
+        $res = api_call('PUT', $this->docs_url(), $data);
+        return json_decode($res->response);
+    }
+
 
     public function delete_document($docid) {
         /*
@@ -215,7 +272,22 @@ class IndexClient {
          * Arguments:
          *     docid: unique document identifier
          */
-        api_call('DELETE', $this->docs_url(), array("docid" => $docid));
+        $res = api_call('DELETE', $this->docs_url(), array("docid" => $docid));
+        return $res->status;
+    }
+
+    public function delete_documents($docids) {
+        /*
+         * Deletes the given docids from the index if they existed. otherwise, does nothing.
+         * Arguments:
+         *     docids: unique document identifiers
+         */
+        $data = array();
+        foreach ($docids as $docid) {
+            $data[] = array("docid" => $docid);
+        }
+        $res = api_call('DELETE', $this->docs_url(), $data);
+        return json_decode($res->response);
     }
 
     public function update_variables($docid, $variables) {
@@ -226,7 +298,8 @@ class IndexClient {
          *     variables: map integer -> float with values for variables that can
          *                later be used in scoring functions during searches.
          */
-        api_call('PUT', $this->variables_url(), array("docid" => $docid, "variables" => convert_to_map($variables)));
+        $res = api_call('PUT', $this->variables_url(), array("docid" => $docid, "variables" => convert_to_map($variables)));
+        return $res->status;
     }
 
     public function update_categories($docid, $categories) {
@@ -236,7 +309,8 @@ class IndexClient {
          *     docid: unique document identifier
          *     categories: map string -> string where each key is a category name pointing to its value
          */
-        api_call('PUT', $this->categories_url(), array("docid" => $docid, "categories" => convert_to_map($categories)));
+        $res = api_call('PUT', $this->categories_url(), array("docid" => $docid, "categories" => $categories));
+        return $res->status;
     }
 
     public function promote($docid, $query) {
@@ -246,12 +320,14 @@ class IndexClient {
          *     docid: unique document identifier
          *     query: the query for which to promote the document
          */
-        api_call('PUT', $this->promote_url(), array("docid" => $docid, "query" => $query));
+        $res = api_call('PUT', $this->promote_url(), array("docid" => $docid, "query" => $query));
+        return $res->status;
     }
 
     public function add_function($function_index, $definition) {
         try {
-            api_call('PUT', $this->function_url($function_index), array("definition" => $definition));
+            $res = api_call('PUT', $this->function_url($function_index), array("definition" => $definition));
+            return $res->status;
         } catch (HttpException $e) {
             if ($e->getCode() == 400) {
                 throw new InvalidDefinition($e->getMessage());
@@ -261,31 +337,59 @@ class IndexClient {
     }
 
     public function delete_function($function_index) {
-        api_call('DELETE', $this->function_url($function_index));
+        $res = api_call('DELETE', $this->function_url($function_index));
+        return $res->status;
     }
 
     public function list_functions() {
         $res = api_call('GET', $this->functions_url());
-        return $res->response;
+        return json_decode($res->response);
     }
 
-    public function search($query, $start=NULL, $len=NULL, $scoring_function=NULL, $snippet_fields=NULL, $fetch_fields=NULL, $category_filters=NULL, $query_variables=NULL) {
+
+    /*
+     * Performs a search.
+     *
+     * @param variables: An array with 'query variables'. Example: array( 0 => 3, 1 => 34);
+     * @param docvar_filters: An array with filters for document variables. 
+     *     Example: array(0 => array(array(1,4), array(6, 9), array(16,NULL)))
+     *     Document variable 0 should be between 1 and 4 OR 6 and 9 OR greater than 16
+     * @param function_filters: An array with filters for function scores. 
+     *     Example: array(2 => array(array(2,6), array(7, 11), array(15,NULL)))
+     *     Scoring function 2 must return a value between 2 and 6 OR 7 and 11 OR greater than 15 for documents matching this query.
+     *
+     */
+    public function search($query, $start=NULL, $len=NULL, $scoring_function=NULL, $snippet_fields=NULL, $fetch_fields=NULL, $category_filters=NULL, $variables=NULL, $docvar_filters=NULL, $function_filters=NULL) {
         $params = array("q" => $query);
         if ($start != NULL) { $params["start"] = $start; }
         if ($len != NULL) { $params["len"] = $len; }
         if ($scoring_function != NULL) { $params["function"] = (string)$scoring_function; }
         if ($snippet_fields != NULL) { $params["snippet"] = $snippet_fields; }
         if ($fetch_fields != NULL) { $params["fetch"] = $fetch_fields; }
-        if ($category_filters != NULL) { $params["category_filters"] = json_encode($category_filters); }
-        
-        if ($query_variables != NULL) {
-            #for k, v in variables.items():
-            #    params['var%d' % int(k)] = str(v)
-        	foreach ($query_variables as $key => $val) {
-        		$params["var".$key ] = $val;
-        	}
+        if ($category_filters != NULL) { $params["category_filters"] = $category_filters; }
+        if ($variables) {
+            foreach( $variables as $k => $v)
+            {
+                $params["var".strval($k)] = $v;
+            }
+        }
+
+        if ($docvar_filters){
+            // $docvar_filters is something like
+            // { 3 => [ (1, 3), (5, NULL) ]} to filter_docvar3 => 1:3,5:*
+            foreach( $docvar_filters as $k => $v){
+                $params["filter_docvar".strval($k)] = implode(array_map( 'map_range', $v), ",");
+            }
         }
         
+        if ($function_filters){
+            // $function_filters is something like
+            // { 2 => [ (1, 4), (7, NULL) ]} to filter_function2 => 1:4,7:*
+            foreach( $docvar_filters as $k => $v){
+                $params["filter_function".strval($k)] = implode(array_map( 'map_range', $v), ",");
+            }
+        }
+
         try {
             $res = api_call('GET', $this->search_url(), $params);
             return json_decode($res->response);
@@ -306,9 +410,22 @@ class IndexClient {
     }
 
     private function refresh_metadata() {
-        $res = api_call('GET', $this->index_url, array());  
+        $res = api_call('GET', $this->index_url, array());
         $this->metadata = json_decode($res->response);
         return $this->metadata;
+    }
+
+    /*
+     * Creates a 'document', useful for IndexClient->add_document and IndexClient->add_documents
+     */
+    private function as_document($docid, $fields, $variables = NULL){
+        if (NULL == $docid) throw new InvalidArgumentException("\$docid can't be NULL");
+        if (mb_strlen($docid, '8bit') > 1024) throw new InvalidArgumentException("\$docid can't be longer than 1024 bytes");
+        $data = array("docid" => $docid, "fields" => $fields);
+        if ($variables != NULL) {
+            $data["variables"] = convert_to_map($variables);
+        }
+        return $data;
     }
 
     private function docs_url()        { return $this->index_url . "/docs"; }
@@ -318,9 +435,6 @@ class IndexClient {
     private function search_url()      { return $this->index_url . "/search"; }
     private function functions_url()   { return $this->index_url . "/functions"; }
     private function function_url($n)  { return $this->index_url . "/functions/". $n; }
-
-
-
     
 }
 
